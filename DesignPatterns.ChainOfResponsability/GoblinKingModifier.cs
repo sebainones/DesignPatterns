a20e460d297ca232ed27a12e24e3158429b45671
Seba Ghaseel <sebainones@ghaseel.com>
2019-11-02 23:09:37 +0100

    Uncommited change

---
+++ DesignPatterns.ChainOfResponsability/GoblinKingModifier.cs
@@ -0,0 +1,13 @@
+ public class GoblinKingModifier : CreatureModifier
+  {
+    public GoblinKingModifier(Game game, Creature creature) : base(game, creature)
+    {
+    }
+
+    protected override void Handle(object sender, Query q)
+    {
+      if (q.CreatureName == creature.Name &&
+          q.WhatToQuery == Query.Argument.Attack)
+        q.Value += 1;
+    }
+  }
